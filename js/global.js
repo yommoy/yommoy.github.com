9e83991e5027ff5ca02872cc2745cb31aa201773
yommoy <yommoy@yeah.net>
2017-06-30 14:46:13 +0800

    优化
    优化

--- js/global.js
+++ js/global.js
@@ -15,19 +15,34 @@ $(document).ready(function() {
         }
     })
 
-    //焦点获取滑动
+    //获取菜单距离左边的位置，保持居中
+    $(".menu").css("left",function() {
+        var menuW =  $(this).width();
+        var bodyW = $(window).width();
+        var leftL = (bodyW - menuW)/2;
+        return leftL;
+    })
+
+    //焦点获取滑动并添加其他事件
     $(".menu>a").each(function() {
         $(this).bind("click",function() {
             var idName = $(this).attr("scroll");
             var scrolTop = $("#"+idName).offset().top;
-            $("body,html").animate({ scrollTop: scrolTop },600,"swing");   
-            if(idName == "section_3") {
-                $("#section_3 .timeLine .experience").addClass("active");
-                $("#section_3 .timeLine svg g ellipse").addClass("active");         
-            } else {
-                $("#section_3 .timeLine .experience").removeClass("active");
-                $("#section_3 .timeLine .lineLoad").removeClass("active");
+            $(".menu>a").removeClass("active");
+            $(this).addClass("active");
+            $("body,html").animate({ scrollTop: scrolTop },600,"swing");    
+            if(idName == "section_2") {
+                $("#section_2 .progress").removeClass("active");
+                setTimeout(function() {
+                    $("#section_2 .progress").addClass("active");
+                },300)       
             }
+            if(idName == "section_3") {
+                $("#section_3 .timeLine").removeClass("active");       
+                setTimeout(function() { 
+                    $("#section_3 .timeLine").addClass("active");     
+                },300)         
+            } 
         })
     })
 })
